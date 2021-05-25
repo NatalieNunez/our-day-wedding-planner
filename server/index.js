932ac5d1ff476517ea279a4abc2d9efd1c990492
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-25 22:27:04 +0000

    Update server/index.js to include a post request to add a new image to the database, tested with HTTPie

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
+// const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
-const staticMiddleware = require('./static-middleware');
+// const staticMiddleware = require('./static-middleware');
+const uploadsMiddleware = require('./uploads-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
-app.use(staticMiddleware);
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.post('/api/uploads', uploadsMiddleware, (req, res, next) => {
+  const imageUrl = `/images/${req.file.filename}`;
+  const sql = `
+  insert into "images" ("url")
+    values ($1)
+    returning *
+    `;
+  const values = [imageUrl];
+  db.query(sql, values)
+    .then(result => {
+      const [newImage] = result.rows;
+      res.status(201).json(newImage);
+    })
+    .catch(err => next(err));
+});
+
+// app.use(staticMiddleware);
 
 app.use(errorMiddleware);
 
