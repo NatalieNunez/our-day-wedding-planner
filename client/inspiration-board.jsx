20b5a585649e21777f4e4ec428b86842a4c920ed
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-26 05:04:24 +0000

    made InspirationBoard component a function rather than a class

--- client/inspiration-board.jsx
+++ client/inspiration-board.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import InspoImages from './inspo-images';
 import UploadPhotos from './upload-photos';
 
-class InspirationBoard extends React.Component {
-  render() {
-    return (
-      <div id='inspo-board'>
-        <span id='inspo-board-header'>Inspiration Board</span>
-        <UploadPhotos />
-        <InspoImages />
-      </div>
-    );
-  }
+function InspirationBoard() {
+  return (
+    <div id='inspo-board'>
+      <span id='inspo-board-header'>Inspiration Board</span>
+      <UploadPhotos />
+      <InspoImages />
+    </div>
+  );
 }
 
 export default InspirationBoard;
