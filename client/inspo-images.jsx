b764de9f2df2177c3292f782fef3ca197c3b3379
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-26 22:30:52 +0000

    removed commented out code

--- client/inspo-images.jsx
+++ client/inspo-images.jsx
@@ -4,21 +4,7 @@ class InspoImages extends React.Component {
   render() {
     return (
       <div className='images-container'>
-        {/* <div className='row'>
-          <img src="../server/public/images/cloud-city.png" alt="placeholder" className="col-third" />
-          <img src="" alt="" className="col-third" />
-          <img src="" alt="" className="col-third" />
-        </div>
-        <div className='row'>
-          <img src="" alt="" className="col-third" />
-          <img src="" alt="" className="col-third" />
-          <img src="" alt="" className="col-third" />
-        </div>
-        <div className='row'>
-          <img src="" alt="" className="col-third" />
-          <img src="" alt="" className="col-third" />
-          <img src="" alt="" className="col-third" />
-        </div> */}
+
       </div>
     );
   }
