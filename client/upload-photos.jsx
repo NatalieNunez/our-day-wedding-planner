b2df91725e2cbe0f91a30c10ae2be36f0268bf19
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-26 17:31:37 +0000

    added click event on UploadPhotos component

--- client/upload-photos.jsx
+++ client/upload-photos.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class UploadPhotos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
   render() {
     return (
       <i className="fas fa-plus upload-icon"></i>
