206e2dcc112dcf33eeebd4aa5c4004bb8ae719b6
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-28 23:30:38 +0000

    created styling for modal

--- client/guest-footer.jsx
+++ client/guest-footer.jsx
@@ -15,12 +15,31 @@ class GuestFooter extends React.Component {
 
   render() {
     return (
+      <>
+      <div id="guest-modal">
+        <form id="guest-form">
+          <div className="modal-btns">
+            <button className="guest-btn cancel">Cancel</button>
+            <button className="guest-btn save" type="submit">Save</button>
+          </div>
+          <div className="row">
+            <div className="divider"></div>
+            <label className="add-guest-label">Add Guest</label>
+            <div className="inputs">
+              <input required autoFocus id="first-name" placeholder="First Name" type="text" />
+              <input required autoFocus id="last-name" placeholder="Last Name" type="text"/>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <div className="guest-footer">
         <h4 className="add-guest-text" onClick={this.handleClick}>Add a guest</h4>
         <button className="add-guest-btn" onClick={this.handleClick}>
           <i className="fas fa-plus add-guest-icon"></i>
         </button>
       </div>
+      </>
     );
   }
 }
--- client/pages/guest-list.jsx
+++ client/pages/guest-list.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../header';
 import GuestFooter from '../guest-footer';
+// import ModalGuest from '../modal-guest';
 
 export default function GuestList(props) {
   return (
     <>
       <Header />
       <GuestFooter />
+      {/* <ModalGuest /> */}
     </>
   );
 }
