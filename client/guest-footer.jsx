c8b00ac0e36ccdeecd2776814aa6a64bc56d869b
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-29 00:10:08 +0000

    Create a modal form to add new guest

--- client/guest-footer.jsx
+++ client/guest-footer.jsx
@@ -16,28 +16,30 @@ class GuestFooter extends React.Component {
   render() {
     return (
       <>
-      <div id="guest-modal">
-        <form id="guest-form">
-          <div className="modal-btns">
-            <button className="guest-btn cancel">Cancel</button>
-            <button className="guest-btn save" type="submit">Save</button>
-          </div>
-          <div className="row">
-            <div className="divider"></div>
-            <label className="add-guest-label">Add Guest</label>
-            <div className="inputs">
-              <input required autoFocus id="first-name" placeholder="First Name" type="text" />
-              <input required autoFocus id="last-name" placeholder="Last Name" type="text"/>
-            </div>
-          </div>
-        </form>
-      </div>
-
-      <div className="guest-footer">
+        <div className="guest-footer">
         <h4 className="add-guest-text" onClick={this.handleClick}>Add a guest</h4>
         <button className="add-guest-btn" onClick={this.handleClick}>
           <i className="fas fa-plus add-guest-icon"></i>
         </button>
+      </div>
+
+      <div id="guest-modal">
+        <div id="guest-modal-box">
+          <form id="guest-form">
+            <div className="modal-btns">
+              <button className="guest-btn cancel">Cancel</button>
+              <button className="guest-btn save" type="submit">Save</button>
+            </div>
+            <div className="row">
+              <div className="divider"></div>
+              <label className="add-guest-label">Add Guest</label>
+              <div className="inputs">
+                <input required autoFocus id="first-name" placeholder="First Name" type="text" />
+                <input required autoFocus id="last-name" placeholder="Last Name" type="text"/>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
       </>
     );
