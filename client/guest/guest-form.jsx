d8868021a457db234c69f1708348a0cd23fddf20
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-29 22:47:15 +0000

    removed dom query, reset form using setState, updated state names for clarity, removed repeated code

--- client/guest/guest-form.jsx
+++ client/guest/guest-form.jsx
@@ -3,21 +3,20 @@ import React from 'react';
 class GuestForm extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.getAllGuests = this.getAllGuests.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
-      isClicked: false,
+      modalOpen: false,
       firstName: '',
       lastName: ''
     };
   }
 
-  handleClick() {
-    const isClicked = this.state.isClicked;
+  toggleModal() {
+    const modalOpen = this.state.modalOpen;
     this.setState({
-      isClicked: !isClicked
+      modalOpen: !modalOpen
     });
   }
 
@@ -27,62 +26,49 @@ class GuestForm extends React.Component {
       this.setState({
         firstName: target.value
       });
+    } else {
+      this.setState({
+        lastName: target.value
+      });
     }
-    this.setState({
-      lastName: target.value
-    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    const form = document.getElementById('guest-form');
     const newGuest = {
-      firstName,
-      lastName
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
     };
     this.props.onSubmit(newGuest);
-    form.reset();
-  }
-
-  getAllGuests() {
-    fetch('/api/guests')
-      .then(res => res.json())
-      .then(guests => {
-        this.setState({
-          guests
-        });
-      });
-  }
-
-  componentDidMount() {
-    this.getAllGuests();
+    this.setState({
+      firstName: '',
+      lastName: ''
+    });
   }
 
   render() {
-    const isClicked = this.state.isClicked;
+    const modalOpen = this.state.modalOpen;
     return (
       <>
         <div className="guest-footer">
-        <h4 className="add-guest-text" onClick={this.handleClick}>Add a guest</h4>
-        <button className="add-guest-btn" onClick={this.handleClick}>
+        <h4 className="add-guest-text" onClick={this.toggleModal}>Add a guest</h4>
+        <button className="add-guest-btn" onClick={this.toggleModal}>
           <i className="fas fa-plus add-guest-icon"></i>
         </button>
       </div>
-      <div id={isClicked ? 'modal-open' : 'guest-modal'}>
+      <div id={modalOpen ? 'modal-open' : 'guest-modal'}>
         <div id="guest-modal-box">
           <form id="guest-form" onSubmit={this.handleSubmit}>
             <div className="modal-btns">
-              <button className="guest-btn cancel" type="button" onClick={this.handleClick}>Cancel</button>
+              <button className="guest-btn cancel" type="button" onClick={this.toggleModal}>Close</button>
               <button className="guest-btn save" type="submit">Save</button>
             </div>
             <div className="row">
               <div className="divider"></div>
               <label className="add-guest-label">Add Guest</label>
               <div className="inputs">
-                <input required autoFocus id="first-name" placeholder="First Name" type="text" name="first-name" onChange={this.handleChange}/>
-                <input required autoFocus id="last-name" placeholder="Last Name" type="text" name="last-name" onChange={this.handleChange}/>
+                <input required autoFocus id="first-name" placeholder="First Name" type="text" name="first-name" value={this.state.firstName} onChange={this.handleChange}/>
+                <input required autoFocus id="last-name" placeholder="Last Name" type="text" name="last-name" value={this.state.lastName} onChange={this.handleChange}/>
               </div>
             </div>
           </form>
