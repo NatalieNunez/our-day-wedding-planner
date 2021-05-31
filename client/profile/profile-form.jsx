06e396b41f890df62366c0a0a91bb4cefe4d1478
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-31 22:43:14 +0000

    Create a ProfileForm component

--- client/profile/profile-form.jsx
+++ client/profile/profile-form.jsx
@@ -5,10 +5,13 @@ class ProfileForm extends React.Component {
     return (
       <div className="profile-page">
         <form className="profile-form">
-          <label>
-            Your Name
-            <input type="text" name="user-name" className="user-name" />
-          </label>
+          <label className="profile-label" >Your Name</label>
+          <input type="text" name="user-name" className="user-name profile-input" placeholder="Your Name" />
+          <label className="profile-label">Partner&apos;s Name</label>
+          <input type="text" name="partner-name" className="partner-name profile-input" placeholder="Partner&apos;s Name" />
+          <label className="profile-label">Wedding Date</label>
+          <input type="date" className="profile-input" />
+          <button type="submit" className="save-profile">Save</button>
         </form>
       </div>
     );
