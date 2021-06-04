3dff3ad0f5a0262a4f003f603000e6e2dcce30dc
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-06-04 18:34:32 +0000

    update data.sql to insert values into budget table

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,5 @@
 insert into "users" ("userName", "partnerName", "weddingDate")
 values ('Steph', 'Jon', 'May 31, 2022');
+
+insert into "budget" ("budgetTotal", "spent", "remaining")
+values (20000, 0, 20000);
