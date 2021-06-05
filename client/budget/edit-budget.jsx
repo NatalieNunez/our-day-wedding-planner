5fe82729daae3e1e2bb7d0f065caea5c08d7d23a
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-06-05 02:54:47 +0000

    styled chart and editBudget component

--- client/budget/edit-budget.jsx
+++ client/budget/edit-budget.jsx
@@ -1,4 +1,38 @@
 import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+const config = {
+  // labels: ['Spent', 'Remaining'],
+  datasets: [
+    {
+      label: 'Budget',
+      backgroundColor: [
+        '#1b9096',
+        '#94dee2'
+      ],
+      hoverBackgroundColor: [
+        '#1b9096',
+        '#94dee2'
+      ],
+      data: [65, 59]
+    }
+  ]
+};
+
+class DoughnutChart extends React.Component {
+  render() {
+    return (
+      <Doughnut
+        data={config}
+        options={{
+          title: {
+            display: false
+          }
+        }}
+      />
+    );
+  }
+}
 
 class EditBudget extends React.Component {
   constructor(props) {
@@ -53,18 +87,33 @@ class EditBudget extends React.Component {
     const editOpen = this.state.editOpen;
     return (
       <div className="budget-div">
-        <label className="budget-label">Total Budget</label>
-        {
-          editOpen
-            ? <form className="budget-form" onSubmit={this.handleSubmit}>
-                <input className="budget-input" type="text" placeholder={`$ ${this.state.budgetTotal}`} onChange={this.handleChange} required/>
-                <button type="submit" className="save-budget">Save</button>
-                <button type="button" className="cancel-budget" onClick={this.toggleForm}>Cancel</button>
-              </form>
-            : <span className="budget-total">{`$ ${this.state.budgetTotal}`}
-                <i className="fas fa-pen edit-budg-icon" onClick={this.toggleForm}></i>
-              </span>
-        }
+        <div className="budget-row">
+          <label className="budget-label">Total Budget</label>
+          {
+            editOpen
+              ? <form className="budget-form" onSubmit={this.handleSubmit}>
+                  <input className="budget-input" type="text" placeholder={`$ ${this.state.budgetTotal}`} onChange={this.handleChange} required/>
+                  <button type="submit" className="save-budget">Save</button>
+                  <button type="button" className="cancel-budget" onClick={this.toggleForm}>Cancel</button>
+                </form>
+              : <span className="budget-total">{`$ ${this.state.budgetTotal}`}
+                  <i className="fas fa-pen edit-budg-icon" onClick={this.toggleForm}></i>
+                </span>
+          }
+        </div>
+        <div className="labels budget-row">
+          <div className="legend-row">
+            <div className="available-legend"></div>
+            <span className="legend-text">Available</span>
+          </div>
+          <div className="legend-row">
+            <div className="spent-legend"></div>
+            <span className="legend-text">Spent</span>
+          </div>
+        </div>
+        <div className="chart budget-row">
+          <DoughnutChart />
+        </div>
       </div>
     );
   }
